7de5e0e8d9fbbb844828c9eca9dd031b41747c91
Sergey Khokhlov <worldfly@yandex.ru>
2014-03-13 11:02:35 +0300

    Create global.js

---
+++ public/global.js
@@ -0,0 +1,13 @@
+
+  function button(n, callback){
+    if(n===2){
+      window.location='video.html';
+    }
+    if(n===1){
+      alert('You already open main page!');
+    }
+   if(n===3){
+     alert('Fuck inline js');
+   }
+   return false; 
+  }
