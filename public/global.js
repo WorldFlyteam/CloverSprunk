d8386f1e2b7e3021bb11807886efedd5b791bbc1
Sergey Khokhlov <worldfly@yandex.ru>
2014-03-27 10:08:28 +0300

    Update global.js

--- public/global.js
+++ public/global.js
@@ -7,7 +7,7 @@
     }
    if(n===3){
      alert('Fuck inline js');
-     getElementById('b-page').style.backgroundColor='red';
+     document.getElementById('b-page').style.backgroundColor='red';
    }
    callback();
    return false; 
