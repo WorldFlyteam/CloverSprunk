b6d95ec3873039517789a57ba42fb89fd5902855
Sergey Khokhlov <worldfly@yandex.ru>
2014-03-27 09:58:34 +0300

    Update global.js

--- public/global.js
+++ public/global.js
@@ -1,9 +1,9 @@
   function button(n, callback){
     if(n===2){
-      window.location='video.html';
+      //smth wrong
     }
     if(n===1){
-      alert('You already open main page!');
+      //smth wrong
     }
    if(n===3){
      alert('Fuck inline js');
